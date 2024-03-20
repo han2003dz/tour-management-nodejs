4f6d777096e1b351b180d15921e397ab2e8c5765
han2003dz <han20031605@gmail.com>
2024-03-20 14:05:07 +0700

    fix header table category

--- src/public/admin/js/data-tables.js
+++ src/public/admin/js/data-tables.js
@@ -2,5 +2,17 @@ window.addEventListener("DOMContentLoaded", (event) => {
   const datatablesSimple = document.getElementById("datatablesSimple");
   if (datatablesSimple) {
     new simpleDatatables.DataTable(datatablesSimple);
+    function handleRemoveParentClass(childClasses) {
+      childClasses.forEach(childClass => {
+        const elements = document.querySelectorAll(`.${childClass}`);
+        elements.forEach(element => {
+          const parent = element.parentNode;
+          parent.removeAttribute("class");
+        });
+      });
+    }
+    const classesToRemove = ["checkAll", "statusAll", "actionAll"];
+    handleRemoveParentClass(classesToRemove);
+    
   }
 });
