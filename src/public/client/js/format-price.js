5dec282645d3c343951b8156edaba055f18c42bb
han2003dz <han20031605@gmail.com>
2024-03-28 16:34:38 +0700

    lấy ra tour nổi bật

---
+++ src/public/client/js/format-price.js
@@ -0,0 +1,17 @@
+// format-price
+const formatPrices = () => {
+  const VND = new Intl.NumberFormat("vi-VN", {
+    style: "currency",
+    currency: "VND",
+  });
+  const prices = document.querySelectorAll("[data-price]");
+  if (prices) {
+    prices.forEach((price) => {
+      if (price.innerHTML) {
+        const priceToNumber = parseInt(price.innerHTML);
+        price.innerHTML = VND.format(priceToNumber);
+      }
+    });
+  }
+};
+formatPrices();
