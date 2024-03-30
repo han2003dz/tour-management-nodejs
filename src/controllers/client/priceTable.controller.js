22ddd00bc81c1f8c655d15e25f849f5b0f3264f3
han2003dz <han20031605@gmail.com>
2024-03-30 10:28:50 +0700

    sort theo tiêu chí tour

--- src/controllers/client/priceTable.controller.js
+++ src/controllers/client/priceTable.controller.js
@@ -1,13 +1,15 @@
 const Tours = require("../../models/tours.model");
 const Categories = require("../../models/categories.model");
 const priceNewHelper = require("../../helpers/priceNew");
-
+const sortHelper = require("../../helpers/sort");
 const index = async (req, res) => {
   try {
+    let sort = {};
+    sortHelper.addToSort(req, sort);
     const tours = await Tours.find({
       status: "active",
       deleted: false,
-    });
+    }).sort(sort);
     const newTours = priceNewHelper.priceNewTours(tours);
     res.render("client/pages/priceTable/index", {
       pageTitle: "Bảng giá tours",
---
+++ src/helpers/sort.js
@@ -0,0 +1,9 @@
+const addToSort = (req, sort) => {
+  if (req.query.sortKey && req.query.sortValue) {
+      sort[req.query.sortKey] = req.query.sortValue;
+  }
+}
+
+module.exports = {
+  addToSort
+};
