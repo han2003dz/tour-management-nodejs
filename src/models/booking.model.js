0a725af93b5a2048bab59214a27965da674293a9
han2003dz <han20031605@gmail.com>
2024-04-05 15:27:37 +0700

    model cart và booking

---
+++ src/models/booking.model.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+const bookingSchema = new mongoose.Schema(
+  {
+    code: String,
+    tours: [
+      {
+        tourId: String,
+        price: Number,
+        discountPercentage: Number,
+        quantity: Number,
+      },
+    ],
+    userInfo: {
+      fullName: String,
+      phone: String,
+      address: String,
+    },
+    note: String,
+    status: String,
+    timeStart: {
+      type: Date,
+      default: Date.now,
+    },
+    createdBy: {
+      account_id: String,
+      createdAt: {
+        type: Date,
+        default: Date.now,
+      },
+    },
+    deleted: {
+      type: Boolean,
+      default: false,
+      required: true,
+    },
+    deletedBy: {
+      account_id: String,
+      deletedAt: Date,
+    },
+    updatedBy: [
+      {
+        account_id: String,
+        updatedAt: Date,
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+const Booking = mongoose.model("Booking", tourSchema, "booking");
+module.exports = Booking;
---
+++ src/models/cart.model.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const cartSchema = new mongoose.Schema(
+  {
+    user_id: String,
+    tours: [
+      {
+        tour_id: String,
+        
+      }
+    ],
+    deleted: {
+      type: Boolean,
+      default: false,
+    },
+    deletedAt: Date,
+    updatedBy: [
+      {
+        account_id: String,
+        updatedAt: {
+          type: Date,
+          default: Date,
+        },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+const Cart = mongoose.model("Cart", cartSchema, "cart");
+module.exports = Cart;
